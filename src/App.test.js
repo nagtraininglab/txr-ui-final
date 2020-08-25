7f4ffd1ed0337508843877cc24ca940c1df23a56
Nagabhushanam <nagabhushanamn@gmail.com>
2020-08-25 21:37:23 +0530

    buiilspec

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headElement = getByText(/TXR - UI/i);
+  expect(headElement).toBeInTheDocument();
 });
